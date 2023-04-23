2cb428f105a19a4282d9c7b121ff89e0d91fdd6e
Emanuele Di Scala <manueldiscala@gmail.com>
2023-04-23 15:24:34 +0200

    feat:(Pari o dispari) style(Palindroma)

--- js/main.js
+++ js/main.js
@@ -6,32 +6,81 @@ function reverse(text) {
     }
     return NuovaParola;
 }
-
+// Funzione di Creazione numero Random
+function getRndnumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 // PALINDROMA
 
 // Inizializzione vabili
 let ParolaUtente = document.getElementById("ParolaUtente");
 let Parolalcontrario;
-let btn = document.getElementById("Testparola");
-
-// Aggiungo Evento click con funzione di controllo della parola
-
-btn.addEventListener("click", function() {
+let btn = document.getElementById("Testparola")
+let dichiarazioneParola; 
+let Palindroma = document.getElementById ("Palindroma"); 
+ // Aggiungo Evento click con funzione di controllo della parol
+btn.addEventListener("click", function () {
     DatoUtente = ParolaUtente.value;
     DatoUtente = DatoUtente.toLowerCase();
-    console.log(DatoUtente);
     Parolalcontrario = reverse(DatoUtente);
-    
     if (Parolalcontrario == DatoUtente) {
-        console.log(Parolalcontrario + " è una parola palindroma");
-        return
-    } else { 
-        console.log("NO");
-    }  
+        dichiarazioneParola = document.createElement("h3");
+        dichiarazioneParola.innerText =`${Parolalcontrario} è una parola palindroma`;
+        Palindroma.append(dichiarazioneParola);
+    } else {
+        dichiarazioneParola = document.createElement("h3");
+        dichiarazioneParola.innerText =`${Parolalcontrario} Non è una parola palindroma`;
+        Palindroma.append(dichiarazioneParola);
+    }
 })
+ // PALINDROM
+ // PARI O DISPARI
+ // Inizializzione vabili
+let SceltaUtente = document.getElementById("NumeroUtente");
+let Sceltapari = document.getElementById("Pari");
+let SceltaDispari = document.getElementById("Dispari");
+let Possibilita = document.getElementById("SceltaPariDispari")
+let Startgo = document.getElementById("letsgo");
+let RandomNumber;
+let Numeroscelto;
+let finalNumber;
+let GenFinalNumber = document.getElementById("GenFinalNumber");
+let Specchietto = document.getElementById("Specchietto");
+let PCresult;
+let genPcNumber = document.getElementById("GenPcNumber");
 
-// PALINDROMA
 
-// PARI O DISPARI
+Startgo.addEventListener("click", function () {
+     // Definsco valore utente
+    Numeroscelto = parseInt(SceltaUtente.value);
+     // Definisco la Scelta del pari o dispari
+    let decisione;
+    decisione = Possibilita.value;
+     // Inserisco le funzioni di Random per user e pc
+    RandomNumber = getRndnumber(1, 5);
+     // Combo per Numeri UTENTI
+    FinalNumber = Numeroscelto + RandomNumber;
+    
+
+    // CONDIZIONE PER DECRETARE IL VINCITORE
+    
+    if (FinalNumber % 2 == Possibilita.value) {
+        console.log("HAI VINTO");
+        let SpamWin = document.createElement("p")
+        SpamWin.innerHTML = "Hai vinto"
+        Specchietto.append(SpamWin)
+        
+    }
+    else if (FinalNumber % 2 !== Possibilita.value) {
+        console.log("HAI PERSO")
+        let SpamLose = document.createElement("p")
+        SpamLose.innerHTML = "Hai perso"
+        Specchietto.append(SpamLose)
+    }
+    GenFinalNumber.innerHTML = FinalNumber;
+    genPcNumber.innerHTML = RandomNumber;
+});
+
+
 
 
